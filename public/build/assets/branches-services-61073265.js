c2428bf2869f73c5be49fdecf9c3f5f04f454de4
Marlou Pepito <marloupepito07@gmail.com>
2023-11-15 09:10:40 +0800

    dakljldlakwdwa

---
+++ public/build/assets/branches-services-61073265.js
@@ -0,0 +1,1 @@
+import{k as a}from"./app-3485f549.js";async function e(){return(await a.get("/api/get_all_branches")).data.status}export{e as g};
---
+++ public/build/assets/exclamation-icon-de346e93.js
@@ -0,0 +1,1 @@
+import{a as o}from"./app-3485f549.js";function n(){return o("svg",{xmlns:"http://www.w3.org/2000/svg",fill:"none",viewBox:"0 0 24 24",strokeWidth:"1.5",stroke:"currentColor",className:"w-6 h-6",children:o("path",{strokeLinecap:"round",strokeLinejoin:"round",d:"M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"})})}export{n as E};
